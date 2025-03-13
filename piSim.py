ffc626165eff4fa1fa5cfc6e0b6abb1e474c5678
mahelyy <mahelyordonez@gmail.com>
2025-03-13 09:16:58 -0400

    Mini project 1

---
+++ piSim.py
@@ -0,0 +1,18 @@
+import sys
+import random
+import math
+
+if __name__ == '__main__':
+	N_values = [10, 100, 1000, 1000000]
+	for N in N_values:
+		inside_circle = 0
+		for _ in range(N):
+			x = random.random()
+			y = random.random()
+			if (x*x + y*y) < 1:
+				inside_circle += 1
+	pi_approx = 4 * inside_circle /N
+	absolute_error = abs(math.pi - pi_approx)
+	relative_error = absolute_error / math.pi
+	print(f'N ={N}, approx pi = {pi_approx}, absolute error = {absolute_error}, relative error = {relative_error}')
+
